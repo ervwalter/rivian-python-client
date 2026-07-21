751dfe318017ebe41fa31811b65269427c7a5626
Erv Walter <erv@ewal.net>
2026-07-20 20:35:37 -0500

    Expose the package version

--- tests/test_version.py
+++ tests/test_version.py
@@ -1,8 +1,10 @@
-"""Test the module version."""
+"""Test the package version."""
 
+import rivian
 from rivian.__version__ import VERSION
 
 
 def test_version() -> None:
     """Test version."""
     assert VERSION != "0.0.0"
+    assert rivian.__version__ == VERSION
